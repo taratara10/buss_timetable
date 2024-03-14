aea8174b7f09cc6e722ceaa5a29b3bcf273a15ab
taratara10 <64339483+taratara10@users.noreply.github.com>
2024-03-14 22:14:49 +0900

    #34 Indicator押下時にpageをスクロールさせる (#37)
    * uiStateをlistenしてpagerを遷移させる
    
    * paddingを下に移す
    
    * indicatorをpadddingではなく、alignで調整
    
    * indicatorを固定幅にする

--- lib/timetable/timetable_screen.dart
+++ lib/timetable/timetable_screen.dart
@@ -20,19 +20,16 @@ class TimetableRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: _AppBarTitle()),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
-        child: Stack(
-          alignment: AlignmentDirectional.bottomCenter,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(top: 42),
-              child: _TimetablePager(),
-            ),
-            _TimetableIndicator(),
-          ],
-        ),
+      appBar: AppBar(title: const _AppBarTitle()),
+      body: const Stack(
+        alignment: AlignmentDirectional.bottomCenter,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 42),
+            child: _TimetablePager(),
+          ),
+          _TimetableIndicator(),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -62,8 +59,18 @@ class _TimetablePager extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final TimetableUiState state = ref.watch(timetableViewModelProvider);
     final viewModel = ref.read(timetableViewModelProvider.notifier);
-    // todo indexの変化でcontrollerのcallbackを呼び出す
     final controller = PageController(initialPage: state.pageIndex);
+
+    /// TimetableUiStateの値が変化したら、呼び出される
+    /// _TimetableIndicatorで変更されたindexにpageを遷移させる
+    ref.listen(timetableViewModelProvider, (previous, next) {
+      controller.animateToPage(
+        next.pageIndex,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.fastEaseInToSlowEaseOut,
+      );
+    });
+
     return PageView.builder(
       controller: controller,
       itemCount: state.timetables.length,
@@ -71,7 +78,9 @@ class _TimetablePager extends ConsumerWidget {
         viewModel.updatePageIndex(page);
       },
       itemBuilder: (context, index) {
-        return TimetableSection(state.timetables[index]);
+        return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: TimetableSection(state.timetables[index]));
       },
     );
   }
@@ -84,19 +93,23 @@ class _TimetableIndicator extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final TimetableUiState state = ref.watch(timetableViewModelProvider);
     final viewModel = ref.read(timetableViewModelProvider.notifier);
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: state.timetables
-          .mapIndexed(
-            (i, item) => _IndicatorCell(
-              isSelected: i == state.pageIndex,
-              name: item.dayType.stringValue,
-              onClick: () {
-                viewModel.updatePageIndex(i);
-              },
-            ),
-          )
-          .toList(),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 48),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        // mainAxisSize: MainAxisSize.min,
+        children: state.timetables
+            .mapIndexed(
+              (i, item) => _IndicatorCell(
+                isSelected: i == state.pageIndex,
+                name: item.dayType.stringValue,
+                onClick: () {
+                  viewModel.updatePageIndex(i);
+                },
+              ),
+            )
+            .toList(),
+      ),
     );
   }
 }
@@ -111,25 +124,22 @@ class _IndicatorCell extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8),
-      child: GestureDetector(
-        onTap: () {
-          onClick();
-        },
-        child: Column(
-          children: [
-            Text(name),
-            const SizedBox(height: 4),
-            (isSelected)
-                ? Container(
-                    height: 3,
-                    width: 42,
-                    color: Theme.of(context).colorScheme.primary,
-                  )
-                : const SizedBox(),
-          ],
-        ),
+    return GestureDetector(
+      onTap: () {
+        onClick();
+      },
+      child: Column(
+        children: [
+          Text(name),
+          const SizedBox(height: 4),
+          Container(
+            height: 3,
+            width: 64,
+            color: (isSelected)
+                ? Theme.of(context).colorScheme.primary
+                : Colors.transparent,
+          )
+        ],
       ),
     );
   }
