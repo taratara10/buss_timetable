c2442af9d580777995879765eb82cbb504d09ff7
taratara10 <shuhei.knhs@gmail.com>
2024-04-03 00:00:25 +0900

    pageIndexを動的に取得

--- lib/timetable/timetable_view_model.dart
+++ lib/timetable/timetable_view_model.dart
@@ -1,8 +1,10 @@
 import 'package:buss_timetable/domain/timetable_repository.dart';
+import 'package:buss_timetable/model/day_type.dart';
 import 'package:buss_timetable/model/station_name.dart';
 import 'package:buss_timetable/model/timetable.dart';
 import 'package:buss_timetable/repository/default_timetable_repository.dart';
 import 'package:buss_timetable/timetable/timetable_ui_state.dart';
+import 'package:clock/clock.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final timetableViewModelNotifierProvider =
@@ -27,12 +29,18 @@ class TimetableViewModel extends StateNotifier<TimetableUiState> {
         .getOrDefault(List.empty());
     state = TimetableUiState(
       stationName: current,
-      pageIndex: 0, // todo
+      pageIndex: _getPageIndex(timetables, clock),
       timetables: timetables,
     );
   }
 
   void updatePageIndex(int index) {
     state = state.copyWith(pageIndex: index);
+  }
+
+  int _getPageIndex(List<Timetable> timetables, Clock clock) {
+    // indexWhere returns -1 if condition is not found.
+    int index = timetables.indexWhere((item) => item.dayType == clock.dayType);
+    return (index >= 0) ? index : 0;
   }
 }
