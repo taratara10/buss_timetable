fc5a6b38671b09092e54bffbac996667f84ad688
taratara10 <64339483+taratara10@users.noreply.github.com>
2024-05-08 21:44:31 +0900

    #58 AppBarに出発駅を追加 (#61)
    * appbarを追加
    
    * footerをscafolldに移動
    
    * timetableも色を変える

--- lib/clock/clock_screen.dart
+++ lib/clock/clock_screen.dart
@@ -1,6 +1,9 @@
 import 'package:buss_timetable/clock/clock_footer_section.dart';
+import 'package:buss_timetable/clock/clock_view_model.dart';
 import 'package:buss_timetable/clock/timeline_section.dart';
+import 'package:buss_timetable/model/station_name.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'clock_card_section.dart';
 
@@ -9,27 +12,46 @@ class ClockRoute extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      appBar: AppBar(
+        title: const _AppBarTitle(),
+        backgroundColor: Theme.of(context).colorScheme.primary.withOpacity(0.3),
+      ),
+      bottomNavigationBar: const Padding(
+        padding: EdgeInsets.only(bottom: 16),
+        child: ClockFooterSection(),
+      ),
+      body: const Center(
         child: Padding(
           padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height: 50),
               ClockCardSection(),
               SizedBox(height: 32),
+              // todo scrollable
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 16),
                 child: TimelineSection(),
               ),
               Expanded(child: SizedBox()),
-              ClockFooterSection(),
-              SizedBox(height: 16),
             ],
           ),
         ),
       ),
     );
+  }
+}
+
+class _AppBarTitle extends ConsumerWidget {
+  const _AppBarTitle();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final StationName title = ref
+        .watch(clockViewModelNotifierProvider)
+        .bottomSheetState
+        .selectedStation;
+    return Text("${title.value} 発");
   }
 }
